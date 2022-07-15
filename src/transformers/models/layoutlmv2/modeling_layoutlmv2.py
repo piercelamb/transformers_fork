e1e215d335fbd9263ca4211e856faa46a710847a
Pierce Lamb <plamb@visotrust.com>
2022-07-14 17:38:38 -0700

    Add PR code to LayoutLMV2ForSequenceClassification too

--- src/transformers/models/layoutlmv2/modeling_layoutlmv2.py
+++ src/transformers/models/layoutlmv2/modeling_layoutlmv2.py
@@ -1014,21 +1014,14 @@ class LayoutLMv2ForSequenceClassification(LayoutLMv2PreTrainedModel):
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        if input_ids is not None and inputs_embeds is not None:
-            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
-        elif input_ids is not None:
-            input_shape = input_ids.size()
-        elif inputs_embeds is not None:
-            input_shape = inputs_embeds.size()[:-1]
-        else:
-            raise ValueError("You have to specify either input_ids or inputs_embeds")
+        input_shape = self._get_input_shape(input_ids, inputs_embeds)
 
         device = input_ids.device if input_ids is not None else inputs_embeds.device
 
         visual_shape = list(input_shape)
         visual_shape[1] = self.config.image_feature_pool_shape[0] * self.config.image_feature_pool_shape[1]
         visual_shape = torch.Size(visual_shape)
-        final_shape = list(input_shape)
+        final_shape = list(self._get_input_shape(input_ids, inputs_embeds))
         final_shape[1] += visual_shape[1]
         final_shape = torch.Size(final_shape)
 
